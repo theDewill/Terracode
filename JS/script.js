6e8080bf9ad89085cc371f1ae721885b90d7b3a0
Nomin <soloriderns@gmail.com>
2022-09-22 10:49:38 +0530

    First Phase Uploaded

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+devbutn = document.getElementById('devbtn');
+devbutn.addEventListner("click",()=>{
+    
+    
+    devbutn.backgroundColor="rgb(255, 72, 0)";
+    devbutn.boxShadow="3px 2px 1px 2px #b14100";
+    devbutn.color="rgb(255, 255, 255)";
+});
